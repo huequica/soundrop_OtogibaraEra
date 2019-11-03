8e64e7cee62e286d007ed256a568f14a3c3bdf19
huequica <harumaki1225@gmail.com>
2019-11-03 22:00:24 +0900

    create callback

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
 
 export function Button(props){
-  const [Pray, setPray] = React.useState(false);
+  const [Play, setPlay] = React.useState(false);
 
-  
+  const togglePlay = React.useCallback(
+    () => setPlay((prev) => !prev)
+  , [setPlay]);
+
   Button.propTypes = {
     button_label: PropTypes.string.isRequired
   };
 
   return(
     <div className='column'>
-      <ReactPlayer url={ props.audio_src } playing={true} />
-      <button className='button is-rounded'>{props.button_label}</button>
+      <ReactPlayer url={ props.audio_src } playing={Play} />
+      <button className='button is-rounded' onClick={togglePlay}>{props.button_label}</button>
     </div>
   );
 }
