105380b5c26db657be9733407fbfc719a5af38dd
huequica <harumaki1225@gmail.com>
2019-11-14 18:04:32 +0900

    make ServiceWorker

---
+++ public/service_worker.js
@@ -0,0 +1,27 @@
+// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    '/poster-keisuke.github.io/',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
