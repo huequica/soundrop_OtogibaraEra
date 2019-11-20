3087cedf6942793bf04aca7a8839ea762ea1f92f
huequica <harumaki1225@gmail.com>
2019-11-20 09:49:44 +0900

    fix indent error

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -4,16 +4,16 @@ import ReactPlayer from 'react-player';
 export function Background(props) {
   return(
     <ReactPlayer
-     className='ReactPlayer'
-     url={ props.url }
-     width=''
-     height=''
-     playing={true}
-     muted={true}
-     playsinline={true}
-     volume={0}
-     controls={false}
-     loop
+      className='ReactPlayer'
+      url={ props.url }
+      width=''
+      height=''
+      playing={true}
+      muted={true}
+      playsinline={true}
+      volume={0}
+      controls={false}
+      loop
     />
   );
 }
