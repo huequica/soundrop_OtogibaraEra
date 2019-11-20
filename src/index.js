da5ce5be6883a8160fbea8ced66decfd45e9777d
huequica <harumaki1225@gmail.com>
2019-11-20 10:06:46 +0900

    fix nospace where foward of comma

--- src/index.js
+++ src/index.js
@@ -40,6 +40,6 @@ window.onload = () => {
   }
   
   ReactDOM.render(<Components.Document />, document.querySelector('div#document'));
-  ReactDOM.render(<App />,document.querySelector('div#app'));
+  ReactDOM.render(<App />, document.querySelector('div#app'));
   ReactDOM.render(<Components.Background url={Background1} />, document.querySelector('div#background'));
 };
